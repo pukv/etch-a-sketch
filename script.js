052dd3aa90507249aec552a499e993f15b7fc11e
pukv <pukvavlovic10@gmail.com>
2024-09-13 23:18:47 +0200

    Added a function which creates a grid 16x16 in this case. Added element size calculation, a for loop which creates the number of divs equal to function parameter ^2. Set the flex basis and height to previously calculated element size + % using ``, ran the function with 16 as a parameter

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector("#container");
+
+function createGrid(gridSize) {
+  container.innerHTML = "";
+
+  const elementSize = 100 / gridSize;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.className = "element";
+    gridElement.style.flexBasis = `${elementSize}%`;
+    gridElement.style.height = `${elementSize}%`;
+
+    container.appendChild(gridElement);
+  }
+}
+
+createGrid(16);
