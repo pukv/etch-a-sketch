8fae62f8615ebdb8897ae2a8a990f94f3c5b59da
pukv <pukvavlovic10@gmail.com>
2024-09-14 00:10:10 +0200

    added h1 element, adjusted margins and added simple styling to the button. Created a new function which uses parse int + prompt to take the value of the new grid and run the function createGrid if the value is bigger than 0 and smaller than 100. Added Event listener to the button to run the newly created function when clicked.

--- script.js
+++ script.js
@@ -28,3 +28,15 @@ createGrid(16);
 const gridBtn = document.createElement("button");
 body.insertBefore(gridBtn, container);
 gridBtn.textContent = "New Grid";
+gridBtn.addEventListener("click", createNewGrid);
+
+function createNewGrid() {
+  const gridSize = parseInt(
+    prompt("Enter a number for squares per side", "16")
+  );
+  if (gridSize > 0 && gridSize < 100) {
+    createGrid(gridSize);
+  } else {
+    alert("Invalid option! Please select a number between 1 & 99.");
+  }
+}
